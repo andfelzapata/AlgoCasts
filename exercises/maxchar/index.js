11bddc4690a70e4bc21a21a1a29e7ef15a7b6f93
Andres Zapata <andreszapatacano@gmail.com>
2018-07-25 21:35:45 -0500

    Add several versions of maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,47 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let charMap = {}
+  let max = 0
+  let maxChar = ""
+  str.split("").forEach(char => {
+    charMap[char] = str.split("").reduce((prev, next) => {
+      if (char === next) {
+        return prev + 1
+      }
+      return prev + 0
+    }, 0)
+  })
 
-module.exports = maxChar;
+  for(let char in charMap) {
+    if(charMap[char] > max) {
+      max = charMap[char]
+      maxChar = char
+    }
+  }
+  return maxChar
+}
+
+function maxCharV2(str) {
+  let charMap = {}
+  let max = 0
+  let maxChar = ""
+
+  for(char of str) {
+    charMap[char] ? charMap[char] ++ : charMap[char] = 1
+  }
+
+  for(char in charMap) {
+    if(charMap[char] > max) {
+      max = charMap[char]
+      charMax = char
+    }
+  }
+  return charMax
+}
+
+module.exports = {
+  maxChar,
+  maxCharV2
+}
--- exercises/maxchar/test.js
+++ exercises/maxchar/test.js
@@ -1,14 +1,31 @@
-const maxChar = require('./index');
+const { maxChar, maxCharV2 } = require("./index")
 
-test('maxChar function exists', () => {
-  expect(typeof maxChar).toEqual('function');
-});
+describe("maxChar", () => {
+  test("maxChar function exists", () => {
+    expect(typeof maxChar).toEqual("function")
+  })
 
-test('Finds the most frequently used char', () => {
-  expect(maxChar('a')).toEqual('a');
-  expect(maxChar('abcdefghijklmnaaaaa')).toEqual('a');
-});
+  test("Finds the most frequently used char", () => {
+    expect(maxChar("a")).toEqual("a")
+    expect(maxChar("abcdefghijklmnaaaaa")).toEqual("a")
+  })
 
-test('Works with numbers in the string', () => {
-  expect(maxChar('ab1c1d1e1f1g1')).toEqual('1');
-});
+  test("Works with numbers in the string", () => {
+    expect(maxChar("ab 1c 1d1e1f1g1 ")).toEqual("1")
+  })
+})
+
+describe("maxCharV", () => {
+  test("maxChar function exists", () => {
+    expect(typeof maxCharV2).toEqual("function")
+  })
+
+  test("Finds the most frequently used char", () => {
+    expect(maxCharV2("a")).toEqual("a")
+    expect(maxCharV2("abcdefghijklmnaaaaa")).toEqual("a")
+  })
+
+  test("Works with numbers in the string", () => {
+    expect(maxCharV2("ab 1c 1d1e1f1g1 ")).toEqual("1")
+  })
+})
